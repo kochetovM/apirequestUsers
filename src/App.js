f8125fed5c3b27744203083254f25d9660f9a11e
misha <easydriverup@gmail.com>
2020-06-16 23:26:32 -0600

    Implement Infinite Scroll

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import Table from "./Table";
 import axios from 'axios';
+import debounce from "lodash.debounce";
 
 function App() {
 
@@ -9,9 +10,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [lastEditModeReset, setLastEditModeReset] = useState(null); //Стейт используется для проверки если уже открыта другая ячейка с формой для редакторования
 
-
-
-    const change_item = (id,item_key,newInput,suite,city,zipcode) => {
+  const change_item = (id,item_key,newInput,suite,city,zipcode) => {
       let index=0;
 
       users.map( (el,i) => {if(el.id==id) index=i} )
@@ -41,11 +40,11 @@ function App() {
         method: 'GET',
         url: 'https://jsonplaceholder.typicode.com/users',
     })
-        .then((response) => {
+    .then((response) => {
           setUsers(response.data);
+          //console.log(response.data);
           setIsLoading(false);
-      });
-
+    }).catch(error => console.log(error));
   };
 
 
@@ -59,6 +58,36 @@ function App() {
       setLastEditModeReset(null);
   } // - вызываем функцию закрытия формы редактирования другой ячейки где открывали а затем сохраняем в стейт нул для обозначения что таких на данный момент больше нет
 
+  window.onscroll = debounce(() => {
+    if (isLoading) return;
+
+    if ((window.innerHeight + document.documentElement.scrollTop)
+        === (document.documentElement.offsetHeight))
+        { loadMore(); }
+  });
+
+  const loadMore = () => {
+      let id = users.length;
+      setIsLoading(true);
+
+      axios({
+          method: 'GET',
+          url: 'https://jsonplaceholder.typicode.com/users',
+      })
+      .then((response) => {
+          const nextUsers = response.data.map((el) => {
+              id++;
+              el.id = id;
+              return el;
+          })
+
+          const newUsers  = users.concat(nextUsers);
+          setUsers(newUsers);
+
+          setIsLoading(false);
+      }).catch(error => console.log(error));
+  }
+
   return (
     <div className="">
         {isLoading ? < button className="btn btn-outline-info header" disabled={isLoading} >Loading..</button>
@@ -67,9 +96,14 @@ function App() {
         <a className="stepahead" href="https://github.com/rusbur/apirequestUsers">github link</a>
 
         <div className="tablestyle">
-        { users[0]==null? "" : <Table lastEditModeReset={lastEditModeReset} runLastEditModeReset={runLastEditModeReset} remember_LastEditModeReset={remember_LastEditModeReset} change_item={change_item} users={users}/>
+        { (users.length==0)? "" : <Table lastEditModeReset={lastEditModeReset} runLastEditModeReset={runLastEditModeReset} remember_LastEditModeReset={remember_LastEditModeReset} change_item={change_item} users={users}/>
         }
         </div>
+
+        {isLoading &&
+         <div className="centerFlex">Loading...</div>
+        }
+
     </div>
   );
 }
